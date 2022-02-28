1a54a3ec1056787fcbdf566f9cbf80086d0f2388
JohnVemula1998 <johnvemula7771998@gmail.com>
2022-02-28 21:28:23 +0530

    Updates

---
+++ precache-manifest.b266b532ba9dbd41a0c493cc336fd95a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b4b37b03e19af18d512",
+    "url": "/react-color-picker/static/css/main.d36f25c4.chunk.css"
+  },
+  {
+    "revision": "6b4b37b03e19af18d512",
+    "url": "/react-color-picker/static/js/main.32e2cef3.chunk.js"
+  },
+  {
+    "revision": "021ed8086a2f9d9156cb",
+    "url": "/react-color-picker/static/js/runtime~main.a264689d.js"
+  },
+  {
+    "revision": "bb13a5f8b9bb86804680",
+    "url": "/react-color-picker/static/js/2.3be7df77.chunk.js"
+  },
+  {
+    "revision": "f9a1e2e293a1fd022166ea9bb22b29b9",
+    "url": "/react-color-picker/index.html"
+  }
+];
